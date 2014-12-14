0fd880abaf323420ebc93028220f4d67a75b76be
Brenton <brent.hall@outlook.com>
2014-12-14 16:54:28 +1300

    Basic splash working

---
+++ Kawaw/Kawaw.Android/SplashScreen.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kawaw.Droid
+{
+    using System.Threading;
+    using Android.App;
+    using Android.OS;
+    using Android.Content.PM;
+
+    [Activity(Theme = "@style/Theme.Splash", MainLauncher = true, NoHistory = true , ScreenOrientation = ScreenOrientation.Portrait)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Wait for 2 seconds
+            Thread.Sleep(30000);
+
+            //Moving to next activity
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
