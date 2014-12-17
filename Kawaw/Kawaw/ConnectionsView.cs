f6333149ecad2808a6ccbc5bbdddfec614b514c4
Tim Penhey <tim@penhey.net>
2014-12-17 20:34:03 +1300

    Connection view bits.

--- Kawaw/Kawaw/ConnectionsView.cs
+++ Kawaw/Kawaw/ConnectionsView.cs
@@ -4,11 +4,75 @@ namespace Kawaw
 {
     class ConnectionsView : BaseView
     {
+        class ConnectionCell : ViewCell
+        {
+            public ConnectionCell()
+            {
+                var name = new Label();
+                name.SetBinding(Label.TextProperty, "Name");
+                var org = new Label();
+                org.SetBinding(Label.TextProperty, "Organisation");
+                // I think this is causing VS to go into an infinite loop.
+                var status = new Label() { HorizontalOptions = LayoutOptions.EndAndExpand};
+                ///var status = new Label();
+                status.SetBinding(Label.TextProperty, "Status");
+
+                var viewLayout = new StackLayout()
+                {
+                    Children =
+                    {
+                        new StackLayout()
+                        {
+                            Orientation = StackOrientation.Horizontal,
+                            Children =
+                            {
+                                name,
+                                status,   
+                            }
+                        },
+                        org,
+                    }
+                };
+
+                View = new ContentView()
+                {
+                    Padding = 10,
+                    Content = new Frame()
+                    {
+                        Padding = 5,
+                        Content = viewLayout,
+                        OutlineColor = Color.Silver,
+                        HasShadow = true
+                    }
+                };
+
+            }
+        }
+
         public ConnectionsView()
         {
             Title = "Connections";
             Icon = "kawaw.png";
-            Content = new Label { Text = "connections view" };
+
+            var list = new ListView()
+            {
+                RowHeight = 70,
+                
+            };
+
+            	
+//var grid = new Grid {
+//Children = { listView, defaultView }
+//listView.Opacity = 0;
+
+            list.SetBinding(ListView.ItemsSourceProperty, "Connections");
+            //list.SetBinding(ListView.SelectedItemProperty, "SelectedItem", BindingMode.TwoWay);
+            // TODO: change this...
+            list.ItemTemplate = new DataTemplate(typeof(ConnectionCell));
+            //list.ItemTemplate.SetBinding(TextCell.TextProperty, "Name");
+            //list.ItemTemplate.SetBinding(TextCell.DetailProperty, "Organisation");
+
+            Content = list;
 
             ToolbarItems.Add(new ToolbarItem("Logout", null, () => MessagingCenter.Send<object>(this, "logout"), ToolbarItemOrder.Secondary));
         }
--- Kawaw/Kawaw/ConnectionsViewModel.cs
+++ Kawaw/Kawaw/ConnectionsViewModel.cs
@@ -1,11 +1,62 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using Xamarin.Forms;
+
 namespace Kawaw
 {
     class ConnectionsViewModel : BaseViewModel
     {
+        private IList<Connection> _connections;
+        private Connection _selectedItem;
+
+        public IList<Connection> Connections
+        {
+            get { return _connections; }
+            private set { SetProperty(ref _connections, value); }
+        }
+
+        public Connection SelectedItem
+        {
+            get { return _selectedItem; }
+            set
+            {
+                SetProperty(ref _selectedItem, value);
+                if (value != null)
+                {
+                    Debug.WriteLine("connection selected {0}", value.Id);
+                    SelectedItem = null;
+                }
+            }
+        }
+
         public ConnectionsViewModel(IApp app)
             : base(app, RootViewModel.Connections)
         {
+            UpdateFromUser(app.User);
+
+            MessagingCenter.Subscribe<object>(this, "connections-updated", delegate
+            {
+                UpdateFromUser(app.User);
+            });
 
         }
+
+        private void UpdateFromUser(RemoteUser user)
+        {
+            if (user == null) return;
+            if (user.Connections == null)
+            {
+                // empty
+                Connections = new ObservableCollection<Connection>();
+                return;
+            }
+            Connections = new ObservableCollection<Connection>(
+                from connection in user.Connections
+                orderby connection.Pending descending, connection.Accepted descending, connection.Organisation, connection.Name
+                select connection);
+        }
+
     }
 }
