56ab5dc7e4ea685d9a1d9cb30c448901429cd56d
Tim Penhey <tim@penhey.net>
2014-12-21 16:55:31 +1300

    Use a Grid for the add email view.

--- Kawaw/Kawaw/AddEmailView.cs
+++ Kawaw/Kawaw/AddEmailView.cs
@@ -31,27 +31,28 @@ namespace Kawaw
                 {
                     VerticalOptions = LayoutOptions.Center,
                     OutlineColor = Device.OnPlatform(Color.Black, Color.White, Color.Blue),
-                    Content = new StackLayout
+                    Content = new Grid
                     {
-                        VerticalOptions = LayoutOptions.Center,
-                        Spacing = 10,
+                        RowSpacing = 10,
+                        RowDefinitions =
+                        {   // Make each of the three rows high enough for the content, not all equal for the biggest.
+                            new RowDefinition{Height = GridLength.Auto},
+                            new RowDefinition{Height = GridLength.Auto},
+                            new RowDefinition{Height = GridLength.Auto}
+                        },
                         Children =
                         {
-                            new Label
                             {
-                                Text = "Add E-Mail address:",
-                                FontSize = size,
-                            },
-                            emailEntry,
-                            new Grid
-                            {
-                                HorizontalOptions = LayoutOptions.End,
-                                Children =
+                                new Label
                                 {
-                                    {cancelButton,0,0},
-                                    {addButton,1,0},
-                                }
+                                    Text = "Add E-Mail address:",
+                                    FontSize = size,
+                                },
+                                0, 3, 0, 1
                             },
+                            {emailEntry, 0, 3, 1, 2},
+                            {cancelButton, 1, 2},
+                            {addButton, 2, 2},
                         }
                     }
                 }
