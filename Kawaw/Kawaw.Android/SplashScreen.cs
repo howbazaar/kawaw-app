a4b11be7370ff34883e722607125db552fb72772
Tim Penhey <tim@penhey.net>
2015-07-19 11:40:55 +1200

    Clean up splashscreen file.

--- Kawaw/Kawaw.Android/SplashScreen.cs
+++ Kawaw/Kawaw.Android/SplashScreen.cs
@@ -1,15 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Android.Content;
-
-namespace Kawaw.Droid
+﻿namespace Kawaw.Droid
 {
-    using System.Threading;
     using Android.App;
     using Android.OS;
-    using Android.Content.PM;
 
     [Activity(Theme = "@style/Theme.Splash", MainLauncher = true, NoHistory = true) ]
     public class SplashActivity : Activity
@@ -20,13 +12,6 @@ namespace Kawaw.Droid
 
             //Moving to next activity
             StartActivity(typeof(MainActivity));
-        }
-        protected override void OnNewIntent(Intent intent)
-        {
-            var tag = intent.GetStringExtra("tag");
-            var id = intent.GetIntExtra("id", 0);
-            System.Diagnostics.Debug.WriteLine("Splash... OnNewIntent: {0}, {1}", tag, id);
-            base.OnNewIntent(intent);
         }
     }
 }
