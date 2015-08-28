1188d037eb1475e4cc6f23dfc01193e953d02eeb
Tim Penhey <tim@penhey.net>
2015-08-29 11:44:51 +1200

    wip

--- Kawaw/Kawaw/ConnectionsView.cs
+++ Kawaw/Kawaw/ConnectionsView.cs
@@ -90,8 +90,6 @@ namespace Kawaw
 
             var list = new ListView()
             {
-                // Surely it could figure this out?
-                // RowHeight = 70,
                 HasUnevenRows = true, // Actually means, go work out the hight of the rows.
             };
 
